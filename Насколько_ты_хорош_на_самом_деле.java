d9c5b55bba8a69e2966ee8823624357838fc9b52
Leonid Bondarenko <leonidbondarenko69@gmail.com>
2023-09-14 23:22:28 +0300

    Насколько_ты_хорош_на_самом_деле

---
+++ Насколько_ты_хорош_на_самом_деле.java
@@ -0,0 +1,26 @@
+public class Насколько_ты_хорош_на_самом_деле {
+
+    public static void main(String[] args) {
+        int[] classPoints = {5, 7, 8, 9};
+        int yourPoints = 8;
+        System.out.println(betterThanAverage(classPoints, yourPoints));
+    }
+    public static boolean betterThanAverage(int[] classPoints, int yourPoints) {
+        int sum = 0;
+        int avarage = 0;
+        for (int i = 0; i < classPoints.length; i++) {
+            sum += classPoints[i];
+        }
+
+        avarage = sum / classPoints.length;
+        if (yourPoints > avarage) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+}
+
+
+
